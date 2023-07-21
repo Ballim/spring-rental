3f77220f1038c507afc7d417b373f37c242de7ec
Ziyaad Ballim <69576388+1828251@users.noreply.github.com>
2023-07-21 22:38:24 +0200

    Init commit

---
+++ src/main/java/co/za/crystalleafdecor/CLD/CldApplication.java
@@ -0,0 +1,13 @@
+package co.za.crystalleafdecor.CLD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/za/crystalleafdecor/CLD/CldApplicationTests.java
@@ -0,0 +1,13 @@
+package co.za.crystalleafdecor.CLD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
